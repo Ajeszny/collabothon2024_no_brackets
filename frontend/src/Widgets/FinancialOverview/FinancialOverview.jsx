56ae347b624c014f7d5b36f2e08542b3d55cc66f
maphy9 <maphy9@gmail.com>
2024-10-19 17:14:29 +0200

    add loading animation

--- frontend/src/Widgets/FinancialOverview/FinancialOverview.jsx
+++ frontend/src/Widgets/FinancialOverview/FinancialOverview.jsx
@@ -5,6 +5,7 @@ import WidgetCloseButton from '../WidgetCloseButton'
 import InteractivePieChart from './Chart';
 import BarChart from './BarChart'
 import DropDown from './DropDown';
+import { BeatLoader } from 'react-spinners';
 
 function FinancialOverview({
   isHidden,
@@ -17,34 +18,53 @@ function FinancialOverview({
   const [loading, setLoading] = useState(true);
 
    
-  //const [data, setData] = useState(false);
-  //fetch("http://localhost:8000/get_balance?currency=EUR", {method: "GET"})
-    //.then((data) => data.json())
-    //.then((json) => setData(json));
+  const handleFetch = () => {
+    setLoading(true);
+    const fetchData = async () => {
+      const data = await fetch(`http://localhost:8000/get_balance?currency=${selectedCurrency}`, {method: "GET"});
+        const json = await data.json();
+      setData(json);
+      setTimeout(() => {
+        setLoading(false);
+      }, 300);
+    };
+    fetchData()
+    .catch(err => {
+      console.error({ err });
+    });
+  }
 
-    const handleFetch = () => {
-      setLoading(true);
-      const fetchData = async () => {
-        const data = await fetch(`http://localhost:8000/get_balance?currency=${selectedCurrency}`, {method: "GET"});
-          const json = await data.json();
-        setData(json);
-        setTimeout(() => {
-          setLoading(false);
-        }, 300);
-      };
-      fetchData()
-      .catch(err => {
-        console.error({ err });
-      });
-    }
+  useEffect(() => {
+    handleFetch();
+  }, []);
 
-    useEffect(() => {
-      handleFetch();
-    }, []);
-
-   if (loading) {
-    return (<div>Loading...</div>);
-   }
+  if (loading) {
+    return (
+    isHidden ?
+    <></> :
+    <div
+    style={ wasPressed ? { opacity: 0 } : {} }
+    className="FinancialOverview"
+    >
+      <WidgetCloseButton setIsHidden={setIsHidden} setWasPressed={setWasPressed} />
+      <div className="Header">
+        <img className="HeaderIcon" src="images/FinancialOverviewIcon.png" />
+        <p className="HeaderTitle">FinancialOverview</p>
+        <DropDown 
+        selectedCurrency={selectedCurrency}
+        setSelectedCurrency={setSelectedCurrency}
+        handleFetch={handleFetch}
+        />
+      </div>
+      <div className="FinancialOverviewContent">
+        <div className="FinancialOverviewChart">
+          <BeatLoader color={"#FED601"}/>
+        </div>
+        <div className="FinancialOverviewBarChart"><BarChart /></div>
+      </div>
+    </div>
+    );
+  }
 
   return (
     isHidden ?
