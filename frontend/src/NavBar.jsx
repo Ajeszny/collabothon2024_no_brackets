47a795f1924ae066d9fcde8918c95f7b6a85828a
anhelin2a <mendohraloanhelina@gmail.com>
2024-10-19 18:55:56 +0200

    navibar updated

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function NavBar() {
   return (
     <div className='NavBar'>
-      <img src="images/commerzbank_logo.png" />
+      <img src="images/commerzbank_logo.png" alt="Commerzbank Logo" />
+      <div className='LoginLock'>
+        <img src="images/LoginLock.png" alt="Lock Icon" />
+        <span className="LoginText">Log out</span> {/* Added Login text */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
